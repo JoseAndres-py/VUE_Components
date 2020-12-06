605bae865aeab16048e33832ede4502d9ff9f229
JoseAndres-py <josean.lopezm@gmail.com>
2020-12-06 15:20:56 -0500

    modificar texto del gooter

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,66 +1,75 @@
 <template>
-<footer class="page-footer bg-green text-black pt-4">
-    <!-- Footer Text -->
-    <div class="container-fluid text-center text-md-left p-4">
-      <!-- Grid row -->
-      <div class="row">
-        <!-- Grid column -->
-        <div class="col-md-6 mt-md-0 mt-3 text-justify">
-          <!-- Content -->
-          <h5 class="text-uppercase font-weight-bold">Ten presente que...</h5>
-          <p>
-            Practicar el “pensamiento computacional” ayuda a aprender a resolver
-            problemas y a procesar grandes volúmenes de información. Estimula la
-            creatividad y aprendizajes vinculados al arte. Ayuda a desarrollar
-            un pensamiento estructurado y a adquirir habilidades creativas y
-            cognitivas que te harán ver los escenarios desde otro punto de
-            vista.
-          </p>
-          <a href="https://github.com/JoseAndres-py."> Repositorio GitHub</a>
+    <!-- Footer -->
+    <footer class="page-footer bg-green text-black pt-4">
+
+        <!-- Footer Text -->
+        <div class="container-fluid text-center text-md-left">
+
+            <!-- Grid row -->
+            <div class="row padding-2">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3 repository-info text-center">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">GitHub Repository</h5>
+                    <p class="text-justify">Practicar el “pensamiento computacional” ayuda a aprender a resolver problemas y a procesar grandes volúmenes de información. Estimula la creatividad y aprendizajes vinculados al arte. Ayuda a desarrollar un pensamiento estructurado y a adquirir habilidades creativas y cognitivas que te harán ver los escenarios desde otro punto de vista.</p>
+                    <a href="https://github.com/JoseAndres-py/semana-2-193" class="text-black"> Repositorio GitHub</a>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none pb-3 ">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mb-md-0 mb-3 text-center">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold ">Team Members</h5>
+                    <table class="member-list" style="width:100%;">
+                        <tr>
+                            <td>Jose Andres</td>
+                            <td>Product Owner</td>
+                            <td><a href="tel:3132926678">+57 3132926678</a></td>
+                        </tr>
+                        <tr>
+                            <td>Luisa Fernanda </td>
+                            <td>Team Member</td>
+                            <td><a href="tel:3102453112">+57 310 2453112</a></td>
+                        </tr>
+                        <tr>
+                            <td>Henry Alejandro</td>
+                            <td>Team Member</td>
+                            <td><a href="3106788653">+57 310 6788653</a></td>
+                        </tr>
+                        <tr>
+                            <td>Alberto Salazar</td>
+                            <td>Team Member</td>
+                            <td><a href="tel:3502347470">+57 350 2347470</a></td>
+                        </tr>
+                        <tr>
+                            <td>Maria Fernanda</td>
+                            <td>Team Member</td>
+                            <td><a href="3127747121">+57 312 7747121</a></td>
+                        </tr>
+                    </table>
+
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
         </div>
-        <!-- Grid column -->
+        <!-- Footer Text -->
 
-        <hr class="clearfix w-100 d-md-none pb-7" />
-
-        <!-- Grid column -->
-        <div class="col-md-6 mb-md-0 mb-3">
-          <!-- Content -->
-          <h5 class="text-uppercase font-weight-bold">Contactanos</h5>
-          <dl class="row">
-            <dt class="col-sm-5 text-muted">Jose Andres Lopez Muñoz</dt>
-            <dd class="col-sm-3">
-              <a href="tel:3132926678">+57 3132926678</a>
-            </dd>
-            <dt class="col-sm-5 text-muted">Luisa Fernanda Castellanos</dt>
-            <dd class="col-sm-3">
-              <a href="tel:3102453112">+57 310 2453112</a>
-            </dd>
-            <dt class="col-sm-5 text-muted">Henry Alejandro Cáceres Téllez</dt>
-            <dd class="col-sm-3">
-              <a href="tel:3106788653">+57 310 6788653</a>
-            </dd>
-            <dt class="col-sm-5 text-muted">Alberto Salazar Mayorga</dt>
-            <dd class="col-sm-3">
-              <a href="tel:3502347470">+57 350 2347470</a>
-            </dd>
-            <dt class="col-sm-5 text-muted">María Fernanda Florez Rodríguez</dt>
-            <dd class="col-sm-3">
-              <a href="tel:3127747121">+57 312 7747121</a>
-            </dd>
-          </dl>
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-1 bg-gray">© 2020 Copyright: DAD
         </div>
-        <!-- Grid column -->
-      </div>
-      <!-- Grid row -->
-    </div>
-    <!-- Footer Text -->
+        <!-- Copyright -->
 
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-1 bg-gray">
-      © 2020 Copyright: DAD
-    </div>
-    <!-- Copyright -->
-</footer>
+    </footer>
+    <!-- Footer -->
 </template>
 
 <script>
