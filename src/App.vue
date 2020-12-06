ef6100d43e0e2d3605f3da8cec8452d1893bfca6
halejotezz <halecatez@gmail.com>
2020-12-06 11:43:53 -0500

    Cambio

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <section-services></section-services>
   </div>
 </template>
 
 <script>
+import SectionServices from './components/SectionServices.vue' 
 
 export default {
   name: 'App',
+  components: {
+    SectionServices
+  }
 }
 </script>
 
