19a7f51b17636cf774786fe3c98bee5509e6a607
JoseAndres-py <josean.lopezm@gmail.com>
2020-12-06 13:21:05 -0500

    Finalizar creacion de componente de equipo VUE

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,73 @@
 <template>
   <div id="app">
+
+    <!-- members -->
+    <div class="container-fluid">
+      <div class="text-center p-3 mb-2 bg-green text-dark title-section justify-content-center">
+            <h2>Equipo de Desarrollo</h2>
+      </div>
+      <div class="row justify-content-center mb-3">
+        <div class="col mt-3"  v-for="(item, index) in team" :key="index" >
+          <team-card v-bind:member="item"> </team-card>
+        </div>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
 <script>
 
+import TeamCard from './components/TeamCard.vue'
+
 export default {
   name: 'App',
+  components : {
+    TeamCard
+  },
+  data() {
+    return {
+      team: [
+        {
+          codigo: 1,
+          nombre: 'Luisa Fernanda Castellanos Rosales',
+          descripcion: 'Ingeniera de Petróleos / Entrenadora de Baloncesto. Edad 28 años. Pasatiempo Montar Bicicleta.',
+          rol: 'Universidad Industrial de Santander / Tunja / Boyacá',
+          image: './img/member1.jpeg'
+        },
+        {
+          codigo: 2,
+          nombre: 'Maria Fernanda Florez Rodríguez',
+          descripcion: 'Estudiante de Bioingenieria / Edad 23 años. Interes en Diversos Lenguajes Java, C, C++, Matlab y R.',
+          rol: 'Universidad de Antioquia / Medellín / Antioquia',
+          image: './img/member2.jpeg'
+        },
+        {
+          codigo: 3,
+          nombre: 'Alberto Salazar Mayorga',
+          descripcion: 'Ingeniero Industrial / Esp. en Dirección de Proyectos / MBA. Edad 31 Años. Pasatiempos Fútbol / Motociclismo.',
+          rol: 'Universidad Sergio Arboleda / Bogotá D.C. / Cundinamarca',
+          image: './img/member3.jpg'
+        },
+        {
+          codigo: 4,
+          nombre: 'Jose Andres Lopez Muñoz',
+          descripcion: 'Estudiante de Ingeniería Electrónica / Edad 22 Años. Apasionado por la I.A. Machine Learning y Cloud Computing.',
+          rol: 'Universidad Santo Tomas / Bogotá D.C. / Cundinamarca',
+          image: './img/member4.jpeg'
+        },
+        {
+          codigo: 5,
+          nombre: 'Henry Alejandro Cáceres Téllez',
+          descripcion: 'Ingeniero De Sistemas / Edad 28 Años. Pasatiempos Fútbol, Baloncesto, Ciclismo, Videojuegos y Anime.',
+          rol: 'Universidad del Tolima / Ibagué / Tolima',
+          image: './img/member5.jpeg'
+        }
+      ],
+    }
+  }
+  
 }
 </script>
 
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,16 +1,37 @@
 <template>
-    <div>
-
+    <div class="box card text-black bg-light">
+    <div class="d-flex justify-content-center p-2">
+            <img v-bind:src= "member.image" v-bind:alt= "member.nombre" class="teamImg mt-3">
+        </div>
+        <div class="card-body text-center">
+            <h5 class="card-title">{{member.nombre}}</h5>
+            <p class="card-role text-center"><i>Desarrollador #{{member.codigo}}</i> </p>
+            <p class="card-text">{{member.descripcion}}</p>
+        </div>
+        <div class="card-footer">
+            <small class="text-black">{{member.rol}}</small>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "TeamCard",
-        props: ['member']
+        props: {
+            'member' : Object
+        }
     }
 </script>
 
 <style scoped>
+
+.card-body > p.card-role{
+    padding: 0%;
+    margin-bottom: 5px;
+}
+
+.card-body > .card-title{
+    margin-bottom: 2px;
+}
 
 </style>
